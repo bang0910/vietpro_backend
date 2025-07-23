85957bbeed5a12772f3ff4be5ec1e02a4baf2a8a
bangg <Phamtrongbang2002.xuantruong@gmail.com>
2025-07-23 21:17:04 +0700

    step 3

--- config/db.js
+++ config/db.js
@@ -2,6 +2,6 @@ module.exports = {
   mongodb: {
     uri:
       process.env.DB_URI ||
-      "mongodb+srv://phamtrongbang2002xuantruong:09102002@cluster0.tdiz6li.mongodb.net/nodejs-api-251?retryWrites=true&w=majority&appName=Cluster0",
+      "mongodb://phamtrongbang2002xuantruong:09102002@ac-zd45ypj-shard-00-00.tdiz6li.mongodb.net:27017,ac-zd45ypj-shard-00-01.tdiz6li.mongodb.net:27017,ac-zd45ypj-shard-00-02.tdiz6li.mongodb.net:27017/nodejs-api-251?ssl=true&replicaSet=atlas-zd45ypj-shard-0&authSource=admin&retryWrites=true&w=majority",
   },
 };
